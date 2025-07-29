ff7c1875c7d182291cc6daaca40af2fb400373ad
Caleb Richards <carichards@umass.edu>
2025-07-29 14:56:44 +0200

    chnage apibase

--- trench-book-reader/public/main.js
+++ trench-book-reader/public/main.js
@@ -7,7 +7,7 @@
 // const apiBase = 'https://sweet-cobras-sit.loca.lt'; // Ngrok URL
 // const apiBase = 'https://192.168.0.54:3000'; // Portable IP
 // const apiBase = 'http://3.65.1.225:3000'; // ← my aws server
-const apiBase = 'https://poggiocivitate.net'; // ← poggio-civitate-project aws server
+const apiBase = 'https://poggiocivitate.net/static-web-showing'; // ← poggio-civitate-project aws server
 
 
 const select = document.getElementById('trenchBookSelect');
