ef24c5ab4059fad85cb8d6883a505da59a5b4ec9
Caleb Richards <carichards@umass.edu>
2025-07-31 08:41:12 +0200

    3D viewer style changes

--- 3d-viewer/main.js
+++ 3d-viewer/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // navbar functionality
     const navbarToggle = document.getElementById('navbarToggle');
     const navbarMenu = document.getElementById('navbarMenu');
     if (navbarToggle) {
@@ -19,4 +20,24 @@ document.addEventListener('DOMContentLoaded', function () {
             localStorage.setItem('theme', newTheme);
         });
     }
+
+
+    // info functionality
+    const infoBtn = document.getElementById('info-btn');
+    const infoModal = document.getElementById('info-modal');
+    const closeInfo = document.getElementById('close-info');
+
+    infoBtn.addEventListener('click', () => {
+        infoModal.classList.remove('hidden');
+    });
+
+    closeInfo.addEventListener('click', () => {
+        infoModal.classList.add('hidden');
+    });
+
+    window.addEventListener('click', (e) => {
+        if (e.target === infoModal) {
+        infoModal.classList.add('hidden');
+        }
+    });
 });
