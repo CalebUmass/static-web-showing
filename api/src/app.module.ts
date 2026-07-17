5b342f419be4cccfae3193e33f16260b30ed60f0
careilly <careilly@umass.edu>
2026-07-17 14:19:41 +0200

    Dumb Guy Alert!

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,5 +3,5 @@ import { PhotosModule } from './photos/photos.module';
 import { CassettaModule } from './cassetta/cassetta.module';
 import { TrenchBookModule } from './trench-book/trench-book.module';
 
-@Module({ imports: [PhotosModule, CassettaModule] })
+@Module({ imports: [PhotosModule, CassettaModule, TrenchBookModule] })
 export class AppModule {}
