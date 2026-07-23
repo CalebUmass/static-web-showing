6d3a6500c7d3def2ba7670ffd5b40ec996a7daad
smeemp <ejblalock17@gmail.com>
2026-07-23 10:48:20 +0200

    ANNOTATION!!!! YAAAYYYY :D

--- oc-search/ocSearchScript.js
+++ oc-search/ocSearchScript.js
@@ -1,65 +1,65 @@
-/* Event listener for enter key trigger */
-document.addEventListener("keyup", function(event){
-    event.preventDefault();
-    if (event.keyCode === 13){
-        document.getElementById('searchButton').click();
-    }
-});
 
-/* An array and dictionary of all sub-filter ids, to fetch their value if needed and append them to the link*/
+/**
+ * -- GLOBAL VARIABLES --
+ * Constant variables are references to elements.
+ * Vars are usually arrays or other iterables to help the code run.
+ */
 
-// subSelects
-const subFilter = document.getElementsByClassName("subSelect");
-var subFilterList = Array.from(subFilter).map(element => element.id);
+// SubSelects (dropdowns which appear under the search bar)
+const subFilter = document.getElementsByClassName("subSelect");         // HTMLCollection of elements
+var subFilterList = Array.from(subFilter).map(element => element.id);   // Array of subSelect IDs
 
-// radioInput
-const radioInput = document.querySelectorAll('input[type="radio"]');
-var radioList1 = Array.from(radioInput).map(element => element.id);
-var radioList2 = Array.from(radioInput).map(element => element.value);
-var radioMap = new Object();                                // This could just be a map????
-for (let i = 0; i < radioList1.length; i++){                // Ok I tried to make it a map and it broke
-    radioMap[`${radioList1[i]}`] = [`${radioList2[i]}`];    // No maps I suppose
+
+// RadioInputs (single-select choices which appear under the search bar)
+const radioInput = document.querySelectorAll('input[type="radio"]');    // HTMLCollection of radio selects
+var radioList1 = Array.from(radioInput).map(element => element.id);     // Array of radio select IDs
+var radioList2 = Array.from(radioInput).map(element => element.value);  // Array of radio select values
+var radioMap = new Object();    // Object similar to a map, associating radiolists to their possible values
+for (let i = 0; i < radioList1.length; i++){
+    radioMap[`${radioList1[i]}`] = [`${radioList2[i]}`];
 }
 
-/* Will add the custom listener function to anything in here (only Object Type at the moment) */
+
+// Selects that will open their own selects
 const selectsWithSubs = Array.from(document.getElementsByClassName("hasSub"));
 const selectsWithSubsIds = new Array(selectsWithSubs.length);
 for (let i = 0; i < selectsWithSubs.length; i++){
     selectsWithSubsIds[i] = selectsWithSubs[i].id;
 }
 
-//debug
-// function click(){
-//     console.log("meow");
-// }
 
-/* To map corresponding selects to their child selects */
+// Map corresponding a select to its list of subSelects
+// NOTE: Currently only includes object-type, as it's the only select besides prime that has subs.
+// The map is included as a solution if other selects ever have children
 var subsMap = new Map();
 let objectTypeChildren = document.getElementById("objectTypeChildren").children;
 let objectTypeChildrenIds = new Array(objectTypeChildren.length);
 
-for (let i = 0; i < objectTypeChildren.length; i++){
+for (let i = 0; i < objectTypeChildren.length; i++){        // Populating ID list
     objectTypeChildrenIds[i] = objectTypeChildren[i].id;
 }
 
-// For now doing this manually since there's only one with subs
-subsMap.set("object-type", objectTypeChildrenIds);
-addListenerToSelect("object-type", objectTypeChildrenIds)
+subsMap.set("object-type", objectTypeChildrenIds);          // Again, just object-type for now
+addListenerToSelect("object-type", objectTypeChildrenIds)   // To make modular in the future!
 
-// We use this so many times it should definitely be a variable
+
+// Element of the main search (searchPrime)
 const searchSelectElement = document.getElementById("searchSelect");
 
-/* Text box where path will appear */ 
+
+// Text box inside the yellow square that will show the search path
 let pathTextBox = document.getElementById("pathText");
 
-/* ArrayList implementation for the created search path */
+
+// Very simplified ArrayList implementation to serve as the container for the user's path
+// NOTE. I DID NOT KNOW ARRAYS ARENT FIXED LENGTH IN JAVASCRIPT. SO THIS CAN LOWKEY. JUST BE AN ARRAY.
 let currentPath = {
     
-    nextIndex: 0,                       // To loop thru use this as length
-    capacity: 10,                       // Size of array (change if needed)
-    list: new Array(this.capacity),     // Size can be bigger/smaller just change it
+    nextIndex: 0,                           // To loop thru use this as length
+    capacity:  5,                           // Size of array (change if needed, but 5 sshould always be enough)
+    list:      new Array(this.capacity),    // Size can be bigger/smaller just change it
 
-    // For use when reset button is clicked
+    // For use when reset button is clicked (clears list)
     reset(){
         this.nextIndex = 0;
         this.list = new Array(this.capacity);
@@ -78,31 +78,49 @@ let currentPath = {
     }
 };
 
-/* Event listener for selecting what to search by */
+
+
+
+/**
+ * -- EVENT LISTENERS --
+ */
+
+// Listens for enter key to be pressed. Then, triggers the search event.
+document.addEventListener("keyup", function(event){
+    event.preventDefault();
+    if (event.keyCode === 13){
+        document.getElementById('searchButton').click();
+    }
+});
+
+
+// Listens for a change in the value of the main search. Then, opens the corresponding dropdown menu,
+// hides the main dropdown menu, and adds selection to path.
+// If PC Number is selected, reveals PC number entry field.
 searchSelectElement.addEventListener("change", function(){
-/*Gets searchSelect value, pulling up the corresponding dropdown and hides searchSelect*/
-    x = searchSelectElement.value;
-    currentPath.add(x);
-    updatePath();
 
-    //pathTextBox.innerText 
+    x = searchSelectElement.value;  // Fetch user-selected value
+    currentPath.add(x);             // Update path with value
+    updatePath();                   // Update text box to display value
 
-        if (x != ""){
+        if (x != ""){ // "" represents the initial value, and means we should not process it
+
+            // Swap visible dropdown
             searchSelectElement.style.visibility = "hidden";
             document.getElementById(`${x}`).style.visibility = "visible";
 
-        /*General sub-filters that apply to every category, makes them visible*/
-
+            // Display the buttons!
             document.getElementById("clearButton").style.visibility = "visible";
             document.getElementById('searchButton').style.visibility = "visible";
 
+            // Apply to all searches except PC lookups
             if (x != "pcnum"){
                 document.getElementById("fragmentSelect").style.visibility = "visible";
                 document.getElementById("conservation-material").style.visibility = "visible";
                 document.getElementById("conservation-action").style.visibility = "visible";
             }
 
-            /*Sub-filters that apply only to biologicalFilters*/
+            // SubSelects that only apply to biological-related finds
             let biologicalFilters = ['taxon', 'element', 'common-name'];
             if (biologicalFilters.includes(searchSelectElement.value)){
                 document.getElementById("preserved").style.visibility = "visible";
@@ -117,7 +135,50 @@ searchSelectElement.addEventListener("change", function(){
 });
 
 
-/* Functions that fetch link based on user input */
+
+
+/**
+ * -- CUSTOM FUNCTIONS w/ ANNOTATIONS --
+ */
+
+/**
+ * Helper function for hasSub selects. Adds a a listener to the parent to detect and open any
+ * child dropdowns according to selection. IDs must be strings.
+ * @param {String} parentID 
+ * The ID of the parent element. Listener will be added to this element.
+ * @param {Iterable<String>} childrenIDList 
+ * An iterable containing all values of the parent that should open a corresponding dropdown.
+ * The value field of the option in the parent should be exactly the id of the corresponding dropdown.
+ */
+function addListenerToSelect(parentID, childrenIDList) {
+    document.getElementById(parentID).addEventListener("change", function(){
+
+        e = document.getElementById(parentID);
+        x = e.value;
+
+        // We know there will only be one match so break unnecessary
+        for (let child of childrenIDList) {
+            if (x == child){
+                e.style.visibility = "hidden";
+                
+                e = document.getElementById(x).style.visibility = "visible";
+
+                currentPath.add(x);
+                updatePath();
+            }
+        }
+    });
+}
+
+
+/**
+ * Generates URL to open based on a PC number entry. PC numbers can be written in the following ways:
+ * - PC 12345678
+ * -  12345678 (space at beginnning)
+ * - 12345678
+ * - PC12345678
+ * @returns URL corresponding to entered PC number.
+ */
 function fetchByPC(){
     pcNumber = document.getElementById("pcnum").value;
     if (pcNumber.includes("PC") && pcNumber.includes(" ")){
@@ -143,60 +204,63 @@ function fetchByPC(){
 }
 
 
-/* Adds a listener to specified select menu to watch for specified selection */
-function addListenerToSelect(parentID, childrenIDList) {
-    document.getElementById(parentID).addEventListener("change", function(){
-
-        e = document.getElementById(parentID);
-        x = e.value;
-
-        // We know there will only be one match so break unnecessary
-        for (let child of childrenIDList) {
-            if (x == child){
-                e.style.visibility = "hidden";
-                
-                e = document.getElementById(x).style.visibility = "visible";
-
-                currentPath.add(x);
-                updatePath();
-            }
-        }
-    });
-}
-
-
-/*Fills in the link dependng on the dropdown and option selected*/
+/**
+ * Generates URL to be opened when a search is performed.
+ * 
+ * - If path is more than two options deep, iterates through the currentPath in order, then
+ * fetches the value in the last dropdown to complete the link.
+ * 
+ * - Otherwise, gets the value of the main select and the next dropdown.
+ * 
+ * Iterates through subSelects to append any remaining filters. Finally, appends the subjects filter
+ * and returns the URL.
+ * @returns Complete URL with all selected filters applied.
+ */
 function typeSearch(){
+    // Current value of main search
     searchType = searchSelectElement.value;
-    let link = `https://opencontext.org/query/?proj=24-murlo&project-map=True&prop=`;
+    let link = `https://opencontext.org/query/?proj=24-murlo&project-map=True&prop=`;   // Base link
 
+    // More than two searches deep 
     if (selectsWithSubsIds.includes(searchType)){
         
-        for (let i = 0; i < currentPath.nextIndex; i++){
+        for (let i = 0; i < currentPath.nextIndex; i++){    // Add all values from the path to link
             link = link.concat(`24-${currentPath.get(i)}---`);
         }
-        link = link.concat(`24-${document.getElementById(currentPath.get(currentPath.nextIndex - 1)).value}`);
+
+        // Fetch and add final value to path, update text to display final path
+        let last = document.getElementById(currentPath.get(currentPath.nextIndex - 1)).value
+        link = link.concat(`24-${last}`);
+        currentPath.add(document.getElementById(currentPath.get(currentPath.nextIndex - 1)).value)
+        updatePath();
         
+    // One or two selects deep
     } else {
+        // Second selected type
         selectedType = document.getElementById(`${searchType}`).value;
 
+        // Add and update path
         currentPath.add(selectedType);
-        console.log(currentPath.list);
         updatePath();
 
-        link = link.concat(`https://opencontext.org/query/?proj=24-murlo&project-map=True&prop=24-${searchType}---24-${selectedType}`);
+        link = link.concat(`24-${searchType}---24-${selectedType}`);
     }
-    /* Calls the subSearch function to look for any sub-filters that was inputted*/
+
+    // Uses subSearch to find applicable sub
     let appendList = subSearch();
     console.log(appendList);
     for (let i = 0; i < appendList.length; i++){
         link = link.concat(`${appendList[i]}`);
     }
+
+    // Done!
     let finalLink = link.concat('&type=subjects#tab=3');
     return finalLink;
 }
 
-/* Button functions */
+/**
+ * Opens a new tab with user's search, whether PC number or select search.
+ */
 function openTab(){
     if (document.getElementById("pcnum").value.trim().length != 0){
         window.open(fetchByPC(), "_blank");
@@ -206,37 +270,54 @@ function openTab(){
     } 
 }
 
+/**
+ * Resets all elements to their default values, clears path, and hides subSelects.
+ */
 function clearSearch(){
+    // Open main select and clear value
     searchSelectElement.style.visibility = "visible";
     searchSelectElement.value = "";
+
+    // Clear all select options thta were modified
     for (let i = 0; i < currentPath.nextIndex; i++){
         document.getElementById(`${currentPath.get(i)}`).style.visibility = "hidden";
         document.getElementById(`${currentPath.get(i)}`).value = "";
     }
+
+    // Hide all subfilters
     for (let i = 0; i < subFilterList.length; i++){
         document.getElementById(`${subFilterList[i]}`).style.visibility = "hidden";
     }
 
+    // Hide radio selects
     document.getElementById("fragmentSelect").style.visibility = "hidden"
     document.getElementById("sexSelect").style.visibility = "hidden"
 
+    // Reset path
     currentPath.reset();
 
+    // Display default path value
     pathTextBox.innerText = "Your search path will appear here!";
 }
 
+
+/**
+ * Brings the user to the projects page of poggiocivitate.net.
+ */
 function returnHome(){
     window.open("https://poggiocivitate.net/projects/all/", "_self");
 }
 
-// The subSearch function appends any extra sub-filter categories to the URL
 
+/**
+ * Iterates through subSelects and radioSelects, and returns all chosen fields.
+ * @returns An array of filter URL entries, for appending to the final URL.
+ */
 function subSearch(){
     let addTo = [];
     for (let i = 0; i < subFilterList.length; i++){
         x = document.getElementById(`${subFilterList[i]}`).value;
         if (x){
-            console.log("a");
             addTo.push(`&prop=24-${subFilterList[i]}---24-${x}`);
         }
     }
@@ -249,11 +330,19 @@ function subSearch(){
     return addTo;
 }
 
+
+/**
+ * Updates path text box with current path.
+ */
 function updatePath(){
+    // Initial path text
     text = `Current Path: ${currentPath.get(0)}`;
+
+    // Add deeper filters
     for (let i = 1; i < currentPath.nextIndex; i++){
         text = text.concat(` : ${currentPath.get(i)}`);
     }
-    console.log(text);
+
+    // Display text
     pathTextBox.innerText = text;
 } 
